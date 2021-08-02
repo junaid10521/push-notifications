6bce1bb9a15e952641f5b94e6dd298a0d9315ac1
junaid10521 <junaidonlygia@gmail.com>
2021-08-02 16:52:15 +0500

    Initial Commit

---
+++ app/Notifications/TypeTwoNotification.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Carbon;
+use NotificationChannels\WebPush\WebPushChannel;
+use NotificationChannels\WebPush\WebPushMessage;
+
+
+class TypeTwoNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database', 'broadcast', WebPushChannel::class];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+//    public function toMail($notifiable)
+//    {
+//        return (new MailMessage)
+//                    ->line('The introduction to the notification.')
+//                    ->action('Notification Action', url('/'))
+//                    ->line('Thank you for using our application!');
+//    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+
+    public function toArray($notifiable)
+    {
+        return [
+            'title' => 'Hello from Notification Type 2!',
+            'body' => 'Thank you for using our Laravel - VueJs application.',
+            'action_url' => 'https://vuejs.com',
+            'created' => Carbon::now()->toIso8601String(),
+        ];
+    }
+
+    /**
+     * Get the web push representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @param  mixed  $notification
+     * @return \Illuminate\Notifications\Messages\DatabaseMessage
+     */
+    public function toWebPush($notifiable, $notification)
+    {
+        return (new WebPushMessage)
+            ->title('Hello from Notification Type 2!')
+            ->icon('/notification-icon.png')
+            ->body('Thank you for using our Laravel - VueJs application.')
+            ->action('View app', 'view_app')
+            ->data(['id' => $notification->id]);
+    }
+}
